a6811672aa03ed7c50223a1c38bbc9ac527f7911
Luiza Szafrańska <szafranska.luiza.b@gmail.com>
2024-12-05 01:25:29 +0100

    I sorted the files into folders and started writing button functions

---
+++ script/Crime-Game.script.js
@@ -0,0 +1,29 @@
+const startPl = document.querySelector('#startPl');
+const startEng =document.querySelector('#startEng');
+
+
+// initialize buttons
+startPl.onclick = goStartPl;
+startEng.onclick = goStartEng;
+
+//functions
+
+function goStartPl() {
+  //document.getElementById("title").style.display = "none";
+  //document.getElementById("description").style.display = "none";
+  //document.getElementById("start-btn").style.display = "none";
+  
+  const keepGoingBtn = document.createElement("button");
+  keepGoingBtn.textContent = "Kontynuuj";
+  keepGoingBtn.style.cursor = "pointer";
+  keepGoingBtn.style.color = "lightgray";
+  keepGoingBtn.style.
+  keepGoingBtn.style.
+  
+  
+  
+  text.textContent = "Noc nad Luthen była czarna jak atrament, przetykana jedynie słabym blaskiem gwiazd. Miasto, spowite mgłą, zdawało się wstrzymywać oddech. W tej ciszy, która bardziej przypominała groźbę niż ukojenie, w rezydencji Eliasza Althorna – wybitnego mistrza magii luster – wydarzyło się coś, co wstrząsnęło podstawami świata.Strażnicy szybko zamknęli miejsce zbrodni, a wiadomość o śmierci Althorna rozeszła się po mieście jak pożar. Ktoś odważył się naruszyć równowagę między światem ludzi a lustrzanym wymiarem. Ktoś zabił jedynego człowieka, który mógł opanować tę moc. Portal pozostaje otwarty, jego światło coraz bardziej zniekształca rzeczywistość, a czas ucieka. Jeśli nie odkryjesz prawdy i nie znajdziesz sprawcy, miasto Luthen stanie się niczym więcej niż pyłem na wietrze – pochłoniętym przez bezdenne lustro. Teraz to Ty stoisz w centrum tej opowieści. Czy dasz radę rozwikłać tajemnicę? Czy odkryjesz, co wydarzyło się tej nocy? Każda decyzja będzie miała swoje konsekwencje. Każdy wybór zbliży Cię do prawdy... lub do zguby. Czas płynie. Śledztwo zaczyna się teraz."};
+
+function goStartEng() {
+
+};
